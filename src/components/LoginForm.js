dbddec6f7b0200925dd151b93776007b9119d635
KaladaJumbo <wkalada1@gmail.com>
2020-10-28 20:36:14 -0400

    final commit

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,25 +1,70 @@
 import React from "react";
 
 class LoginForm extends React.Component {
-  constructor() {
+  constructor(props) {
     super();
 
-    this.state = {};
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+
+  submitHandler = () => {
+    let data = {username: this.state.username, password: this.state.password}
+    this.props.handleLogin(data)
+  }
+
+  // handleUsername = (e) => {
+  //   this.setState({
+  //     username: e.target.value
+  //   })
+  // }
+
+  // handlePassword = (e) => {
+  //   this.setState({
+  //     password: e.target.value
+  //   })
+  // }
+
+  handleForm = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit= {(e) => {
+        e.preventDefault();
+        this.state.password.length > 0 && this.state.username.length > 0 ?
+        this.submitHandler()
+        :
+        null
+      }
+        }>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input 
+            id="username" 
+            name="username" 
+            type="text"
+            value= {this.state.username}
+            onChange= {(e) => {this.handleForm(e)}}
+             />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input 
+            id="password" 
+            name="password" 
+            type="password"
+            value= {this.state.password}
+            onChange= {(e) => {this.handleForm(e)}}
+             />
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,17 +1,32 @@
 import React from "react";
 
 class TwitterMessage extends React.Component {
-  constructor() {
+  constructor(props) {
     super();
 
-    this.state = {};
+    this.state = {
+      value: "",
+      wordLimit: props.maxChars
+    };
+  }
+
+  changeState = (e) => {
+    this.setState({
+      value: e.target.value
+    })
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" 
+        value= {this.state.value} 
+        onChange = {(e) => {this.changeState(e)}}
+        name="message" 
+        id="message"
+         />
+         <h2>word limit: {this.state.wordLimit - this.state.value.length}</h2>
       </div>
     );
   }
